079599969cf46f1097210240db7dbe6872dfef7b
BorisELazarov <boriselazarov@gmail.com>
2024-08-11 22:31:03 +0300

    Reworked the mapping and the dtos so that we have separate dtos for creating and editing/potentially creating.

--- dndCharacterCreatorDemo/src/main/java/com/example/dndcharactercreatordemo/bll/dtos/spells/ReadSpellDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.dndcharactercreatordemo.bll.dtos.spells;
-
-public record ReadSpellDTO(Long id, Boolean isDeleted,
-                           String name, int level,
-                           String castingTime, int castingRange,
-                           String target, String components,
-                           int duration, String description){
-}
--- dndCharacterCreatorDemo/src/main/java/com/example/dndcharactercreatordemo/bll/dtos/spells/SaveSpellDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.dndcharactercreatordemo.bll.dtos.spells;
-
-public record SaveSpellDTO(Boolean isDeleted,
-                           String name, int level,
-                           String castingTime, int castingRange,
-                           String target, String components,
-                           int duration, String description){
-}
